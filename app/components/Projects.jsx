b45c16289ec9b3237d09b296ba6e1d93620b5c1b
Marielle Paitan <149914734+DevYelli@users.noreply.github.com>
2025-08-26 13:08:32 +0800

    Update Projects.jsx

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -15,7 +15,7 @@ const Projects = () => {
                 <div className="border-2 border-[#929AAB] p-4 sm:p-6 flex flex-col items-start
                     cursor-pointer transition duration-500 
                     hover:shadow-[4px_4px_6px_rgba(0,0,0,0.3)] hover:bg-[#EEEEEE] hover:-translate-y-1
-                    min-h-[400px]">
+                    min-h-[auto]">
                 <h3 className="text-[28px] md:text-[32px] md:text-[50px] mb-4 mt-5 text-gray-700 text-left w-full break-words leading-snug">Software Engineering Project</h3>
                 <p className="text-[14px] sm:text-[16px] md:text-[20px] text-gray-600 leading-6 mb-2 text-left w-full mt-4">
                     During our Software Engineering course, my team developed a Facial Recognition Attendance Management System.
@@ -41,7 +41,7 @@ const Projects = () => {
                 <div className="border-2 border-[#929AAB] p-4 sm:p-6 flex flex-col items-start
                     cursor-pointer transition duration-500 
                     hover:shadow-[4px_4px_6px_rgba(0,0,0,0.3)] hover:bg-[#EEEEEE] hover:-translate-y-1
-                    min-h-[400px]">
+                    min-h-[auto]">
 
                     <h3 className="text-[28px] sm:text-[32px] md:text-[50px] mb-4 mt-5 text-gray-700 text-left w-full break-words">
                         Game Development
