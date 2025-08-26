c631f71d31d4185c1a5c16e00dff5a5f404d9994
Marielle Paitan <149914734+DevYelli@users.noreply.github.com>
2025-08-26 13:07:18 +0800

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,16 +3,8 @@
 import React, { useRef, useEffect, useState } from 'react'
 import Image from 'next/image'
 const Navbar = () => { 
-    const sideMenuRef = useRef();
+    const [menuOpen, setMenuOpen] = useState(false);
     const [currentTime, setCurrentTime] = useState(new Date());
-
-    const openMenu = () => {
-        sideMenuRef.current.style.transform = 'translateY(0)'
-    }
-
-    const closeMenu = () => {
-        sideMenuRef.current.style.transform = 'translateY(-100%)'
-    }
 
     const dateString = currentTime.toLocaleDateString([], { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' });
 
@@ -35,24 +27,26 @@ const Navbar = () => {
                 <a href='#contact' className='hidden lg:flex items-center gap-6 px-10 py-2.5 hover:font-bold transition-all duration-200"'>
                     Get in Touch!
                 </a>
-                <button className='block md:hidden ml-3' onClick={openMenu}>
-                    <Image src='/menu.png' alt='menu' width={40} height={40} className='cursor-pointer'></Image>
+                <button className='block md:hidden ml-3' onClick={() => setMenuOpen(true)}>
+                    <Image src='/menu.png' alt='menu' width={30} height={30} className='cursor-pointer'></Image>
                 </button>
             </div>
 
             {/* MOBILE MENU */}
-            <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-10 py-30 px-20 fixed top-0 left-0 w-screen h-120
-             z-50 bg-black/90 transition-transform duration-500 ease-in-out 
-             font-bold text-xl text-white transform -translate-y-full">
+            <ul className={`flex md:hidden flex-col gap-10 py-8 px-5 fixed top-0 left-0 w-screen h-100
+          z-50 bg-black/90 transition-transform duration-500 ease-in-out 
+          font-bold text-xl text-white transform pl-15 pt-20 ${
+            menuOpen ? "translate-y-0" : "-translate-y-full"
+          }`}>
                 
-                <div className='absolute right-5 top-6' onClick={closeMenu}>
+                <div className='absolute right-5 top-6' onClick={() => setMenuOpen(false)}>
                     <Image src='/close.png' alt='' width={50} height={40} className='cursor-pointer'></Image>
                 </div>
                 
-                <li><a href='#top' onClick={closeMenu} >Home</a></li>
-                <li><a href='#about' onClick={closeMenu} >About me</a></li>
-                <li><a href='#projects' onClick={closeMenu} >Projects</a></li>
-                <li><a href='#projects' onClick={closeMenu} >Get in touch</a></li>
+                <li><a href='#top' onClick={() => setMenuOpen(false)} >Home</a></li>
+                <li><a href='#about' onClick={() => setMenuOpen(false)} >About me</a></li>
+                <li><a href='#projects' onClick={() => setMenuOpen(false)} >Projects</a></li>
+                <li><a href='#contact' onClick={() => setMenuOpen(false)} >Get in touch</a></li>
             </ul>
 
         </nav>
